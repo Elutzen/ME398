1e078544ef6688db161cf7420928a076841e5fac
Elutzen <Elutzen@gmail.com>
2017-04-27 15:34:09 -0500

    more

--- q2_full_functional/q2_full_functional.ino
+++ q2_full_functional/q2_full_functional.ino
@@ -1,9 +1,53 @@
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
 }
+
+void voicePWM(){
+  //drives voice coil with pdm signal
+}
+
+void getCurrent(){
+  //reads the current from the voice coil
+}
+
+void correctPosition(){
+  //moves voice coil into reading position
+}
+
+void changeCurrent(int sign, float amount){
+  //changes the current in the direction of the sign(sign 1 or -1) and by the amount (amount)
+}
+
+void tare(){
+  //corrects position and sets the tare current
+}
+
+void measure(){
+ //corrects position and measures current to find mass 
+}
+
+float calcMass(float I){
+  //takes in a current and returns a mass
+  float massMeasured;
+  return massMeasured;
+}
+
+void auger(){
+  
+}
+
+void dump(){
+  
+}
+
+void dispense(){
+  
+}
+
+
+

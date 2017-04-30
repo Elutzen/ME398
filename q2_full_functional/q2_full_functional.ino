51c53286dde07e92f69e2856f6b5a134c32564d5
Elutzen <Elutzen@gmail.com>
2017-04-30 13:13:08 -0500

    playing with pwm

--- q2_full_functional/q2_full_functional.ino
+++ q2_full_functional/q2_full_functional.ino
@@ -1,52 +1,63 @@
-void setup() {
-  
+int analogPin = 0;
 
+void setup() {
+  pinMode(5, OUTPUT);//voice coil pwm
+  pinMode(4, OUTPUT);//voice coil enable
+  Serial.begin(9600);
+  digitalWrite(4,HIGH);
 }
 
 void loop() {
- 
+  getCurrent();
+  voicePWM();
 }
 
-void voicePWM(){
+void voicePWM() {
   //drives voice coil with pdm signal
+    digitalWrite(5, HIGH);
+    delayMicroseconds(1); // Approximately 50% duty cycle @ 5KHz
+    digitalWrite(5, LOW);
+    delayMicroseconds(10 - 5);
 }
 
-void getCurrent(){
+void getCurrent() {
   //reads the current from the voice coil
+  int val = analogRead(analogPin);
+  Serial.println(val);
 }
 
-void correctPosition(){
+void correctPosition() {
   //moves voice coil into reading position
 }
 
-void changeCurrent(int sign, float amount){
+void changeCurrent(int sign, float amount) {
   //changes the current in the direction of the sign(sign 1 or -1) and by the amount (amount)
 }
 
-void tare(){
+void tare() {
   //corrects position and sets the tare current
 }
 
-void measure(){
- //corrects position and measures current to find mass 
+void measure() {
+  //corrects position and measures current to find mass
 }
 
-float calcMass(float I){
+float calcMass(float I) {
   //takes in a current and returns a mass
   float massMeasured;
   return massMeasured;
 }
 
-void auger(){
-  
+void auger() {
+
 }
 
-void dump(){
-  
+void dump() {
+
 }
 
-void dispense(){
-  
+void dispense() {
+
 }
 
 

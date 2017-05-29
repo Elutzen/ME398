65e8555264184fb8788c49ea057e28653fa21349
A Martin <a-martin@nusolar.org>
2017-05-28 23:03:38 -0500

    New Button Objects and sample behavior

--- AffyUI/AffyUI.ino
+++ AffyUI/AffyUI.ino
@@ -25,7 +25,9 @@
 
 #define _CS 10
 #define _DC 9
+#define _BRIGHT 7
 Adafruit_HX8357 tft = Adafruit_HX8357(_CS,_DC);
+short TFTbrightness = 127;
 
 // For better pressure precision, we need to know the resistance
 // between X+ and X- Use any multimeter to read it
@@ -35,60 +37,48 @@ TSPoint p;
 bool isTouching;
 
 /////// Define buttons and their actions  ////////
+enum BtnType     {SINGLE,TOGGLE,ONPRESS,ONRELEASE}; 
+enum BtnState    {RELEASED,PRESSED};
 struct Button {
   int16_t x,y,w,h;
   char* label;
   uint16_t color;
-  void draw() {
-    tft.fillRect(x,y,w,h,color);
-    tft.setFont();
-    tft.setCursor(x+5,y+5);
-    tft.setTextColor(HX8357_BLACK);
-    tft.setTextSize(3);
-    tft.print(label);
-  }
-  bool pressed() { return (isTouching) && (p.x > x && p.y > y && p.x < x+w && p.y < y+h); };
-  Button(int16_t _x, int16_t _y, int16_t _w, int16_t _h, char* _label, uint16_t _color) : x(_x), y(_y), w(_w), h(_h), label(_label), color(_color) {};
+  BtnType type;
+  BtnState state = RELEASED, prevState = RELEASED;
+  bool togState = 0;
+  long millisLastChanged;
+
+  // State Functions
+  const inline bool pressed()      { return state==PRESSED; }
+  const inline bool justPressed()  { return state==PRESSED && prevState == RELEASED; }
+  const inline bool justReleased() { return state==RELEASED && prevState == PRESSED; }
+  const inline bool released()     { return state==RELEASED; }
+  const inline bool tapped()       { return (state==RELEASED) && (prevState==PRESSED) && (millis() - millisLastChanged < 500);}
+  const inline bool toggledOn()    { return togState; }
+  const inline bool stateChanged() { return state != prevState; }
+
+  // Routines
+  void update();
+  const void draw();
+
+  // CTOR
+  Button(BtnType _type, int16_t _x, int16_t _y, int16_t _w, int16_t _h, char* _label, uint16_t _color) : 
+    x(_x), 
+    y(_y), 
+    w(_w), 
+    h(_h), 
+    label(_label), 
+    color(_color), 
+    type(_type)
+    {};
 };
 
-Button BtnDispense(50, 50, 150, 50, "Dispense",HX8357_GREEN);
-Button BtnCancel  (50, 110, 150, 50, "Cancel", HX8357_RED);
+// Create buttons here
+Button BtnDispense(BtnType::SINGLE, 50, 50, 150, 50, "Dispense",HX8357_GREEN);
+Button BtnCancel  (BtnType::TOGGLE, 50, 110, 150, 50, "Cancel", HX8357_RED);
+Button BtnRapid   (BtnType::SINGLE, 50, 170, 150, 50, "Rapid", HX8357_YELLOW);
 
-
-////////// MAIN CODE /////////////
-void setup(void) {
-  Serial.begin(115200);
-  tft.begin(HX8357D);
-  tft.fillScreen(HX8357_BLACK);
-  BtnDispense.draw();
-  BtnCancel.draw();
-}
-
-void loop(void) {
-  handleTouch();
-  
-  // we have some minimum pressure we consider 'valid'
-  // pressure of 0 means no pressing!
-  if (isTouching) {
-     Serial.print("X = "); Serial.print(p.x);
-     Serial.print("\tY = "); Serial.print(p.y);
-     Serial.print("\tPressure = "); Serial.println(p.z);
-     drawReadout();
-  }
-
-  if (BtnDispense.pressed()) {
-    BtnDispense.color = HX8357_BLUE;
-    BtnDispense.draw();
-  }
-
-  if (BtnCancel.pressed()) {
-    BtnCancel.color = HX8357_BLUE;
-    BtnCancel.draw();
-  }
-  
-  delay(20);
-}
-
+/////////// HELPERS //////////////
 void handleTouch() {
   // a point object holds x y and z coordinates
   p = ts.getPoint();
@@ -99,12 +89,103 @@ void handleTouch() {
   isTouching = (p.z > 0 && p.z < 100);
 }
 
+void updateButtons() {
+  BtnDispense.update();
+  BtnCancel.update();
+  BtnRapid.update();
+}
+
+void drawAllButtons(bool force = false) {
+  /* DISPENSE */
+  if (BtnDispense.millisLastChanged > millis() - 300 || force) {
+    BtnDispense.color = BtnDispense.justReleased() ? HX8357_BLUE: HX8357_GREEN;
+    BtnDispense.draw();
+  }
+
+  /* CANCEL */
+  if (BtnCancel.stateChanged() || force) {
+    BtnCancel.color = BtnCancel.toggledOn() ? HX8357_BLUE : HX8357_RED;
+    BtnCancel.draw();
+  }
+
+  /* RAPID */ 
+  if (BtnRapid.stateChanged() || force) {
+    BtnRapid.color = BtnRapid.pressed() ? HX8357_BLUE : HX8357_YELLOW;
+    BtnRapid.draw();
+  }
+}
+
 void drawReadout() {
   tft.setFont(&FreeSansBold24pt7b);
-  tft.setCursor(50,300);
+  tft.setCursor(50,350);
   tft.setTextColor(HX8357_WHITE);
   tft.setTextSize(2);
-  tft.fillRect(50,200,200,106,HX8357_BLACK);
+  tft.fillRect(50,250,200,106,HX8357_BLACK);
   tft.print(p.x/100.0);
+  tft.drawCircle(p.x,p.y,3,HX8357_RED);
+}
+
+////////// MAIN CODE /////////////
+void setup(void) {
+  Serial.begin(115200);
+  pinMode(_BRIGHT,OUTPUT); // PWM Brightness pin
+  tft.begin(HX8357D);
+  tft.fillScreen(HX8357_BLACK);
+  Serial.print(BtnDispense.justPressed());
+  drawAllButtons(/*FORCE UPDATE*/ true);
+  drawReadout();
+}
+
+void loop(void) {
+  handleTouch();
+  updateButtons();
+  drawAllButtons();
+  TFTbrightness = BtnCancel.toggledOn() ? 20 : 255;
+  analogWrite(_BRIGHT,TFTbrightness); // Controll LCD brightness
+  
+  // we have some minimum pressure we consider 'valid'
+  // pressure of 0 means no pressing!
+  if (isTouching) {
+     Serial.print("X = "); Serial.print(p.x);
+     Serial.print("\tY = "); Serial.print(p.y);
+     Serial.print("\tPressure = "); Serial.println(p.z);
+     drawReadout();
+  }
+     
+  delay(10);
+}
+
+// Button update code
+void Button::update() {
+  // Sync current state
+  prevState = state;
+
+  // Update Current State
+  if ((isTouching) && (p.x > x && p.y > y && p.x < x+w && p.y < y+h))
+  {
+    state = PRESSED;
+  } else {
+    state = RELEASED;
+  }
+
+  if (prevState != state) {millisLastChanged = millis();}
+
+  // Update toggle state
+  if (type == TOGGLE)
+  {
+    if (justPressed())
+    {
+      togState = !togState;
+    }
+  }
+}
+
+const void Button::draw() {
+  tft.fillRect(x,y,w,h,color);
+  tft.setFont();
+  tft.setCursor(x+5,y+5);
+  tft.setTextColor(HX8357_BLACK);
+  tft.setTextSize(3);
+  tft.print(label);
 }
 

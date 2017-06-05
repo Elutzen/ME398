b0f7d648e49749c09c9a4cbaa62ab450dee049c7
Asaurus1 <Asaurus1@users.noreply.github.com>
2017-06-05 00:25:27 -0500

    Added auger code

--- Final/Final.ino
+++ Final/Final.ino
@@ -4,7 +4,7 @@
 #include <DRV8834.h>
 #include <avr/wdt.h>
 #include <Arduino.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <SPI.h>
 
 //Overall Control
@@ -28,10 +28,15 @@ int dacVal = 50000000;
 
 
 //auger
+#define AUG_FASTRPM 300
+#define AUG_SLOWRPM 10
+long rpm = 60;
+int tempRpm = 0;
+int numSteps = 0;
 // using a 200-step motor (most common)
 // pins used are DIR, STEP, MS1, MS2, MS3 in that order
-DRV8825 stepper(200, 8, 9);
-#define ENBL 7
+DRV8825 stepper(200, 30, 29);
+#define ENBL 28
 #define enable() digitalWrite(ENBL,LOW)
 #define disable() digitalWrite(ENBL,HIGH);
 
@@ -50,30 +55,39 @@ void setup() {
   Serial.begin(9600);
 HWSERIAL.begin(9600);
 
+  //Auger setup
+  pinMode(ENBL, OUTPUT);
+  disable();
+  stepper.setRPM(rpm);
+  stepper.setMicrostep(1);
 
   //Dac set up
   analogWriteResolution(10);
   analogWriteFrequency(4, 46875);
   Serial.println("START");
 
-
-
 }
 
 void loop() {
-  //float val = getPosition();
+  float val = getPosition();
   //Serial.println(val);
   //readCurrent();
-  correctPosition();
+  //correctPosition();
   //analogWrite(PWMpin,400);
+
+  readCommand();
   switch (action) {
     case 'a' :
-      //Serial.println("Current Action: Stopped\n" );
       disable();
       action = 'a';
       break;
+    case 'k':
+      augerSlow();
+      break;
+    case 'p':
+      plateVibe();
+      break;
   }
-  //menu();
 }
 
 
@@ -144,12 +158,20 @@ int getPosition() {
 
 
 */
-void augerFast() {
+//auger
+void takeSteps(int s) {
+  stepper.setRPM(rpm);
+  stepper.move(s);
+}
 
+void augerFast() {
+  stepper.setRPM(AUG_FASTRPM);
+  stepper.move(10);
 }
 
 void augerSlow() {
-
+  stepper.setRPM(AUG_SLOWRPM);
+  stepper.move(10);
 }
 
 /* End of Auger
@@ -216,22 +238,78 @@ float calcMass() {
 
 */
 
-void serialRead() {
+void readCommand() {
   if (Serial.available()) {
     //Serial.flush();
     char c = Serial.read();
     switch (c) {
       case 'a' :
+        Serial.println("Current Action: Stopped\n" );
+        /*setPWM(255);*/
         action = 'a';
         break;
-      case 'b':
-        action = 'b';
+      case 'b' :
+        /*Serial.println("Set PWm (int)\n" );
+        while (!Serial.available()) {}
+        tempPWM = Serial.parseInt();
+        if (tempPWM < 256) {
+          setPWM(tempPWM);
+        }
+        else {
+          Serial.println("Invalid int\n" );
+        }
+        break;*/
+      case 'c' :
+        /*Serial.print("current is ");
+        Serial.println(getCurrent());*/
         break;
-      case 'c':
-        action = 'c';
+      case 'd' :
+        /*Serial.print("position is ");
+        Serial.println(getPosition());*/
         break;
-      case'd':
-        action = 'd';
+      case 'e' :
+        Serial.print("correcting position ");
+        correctPosition();
+        break;
+      case 'f' :
+        Serial.print("measure mass (in current/ not tared) ");
+        /*correctPosition();
+        temp = String(calcMass());
+        Serial.println(temp);*/
+        break;
+      case 'g' :
+        Serial.println("Number of steps (int)  200 steps = 1 rev\n");
+        while (!Serial.available()) {}
+        numSteps = Serial.parseInt();
+        enable();
+        takeSteps(numSteps);
+        disable();
+        break;
+      case 'h' :
+        /*Serial.print("tare ");
+        tare();*/
+        break;
+      case 'i' :
+        Serial.print("enable ");
+        enable();
+        break;
+      case 'j' :
+        Serial.print("disable ");
+        disable();
+        break;
+      case 'k' :
+        Serial.print("auger");
+        enable();
+        action = 'k';
+        break;
+      case 'l' :
+        Serial.println("rpm (int)  ");
+        while (!Serial.available()) {}
+        rpm = Serial.parseInt();
+        break;
+      case 'm':
+        enable();
+        action = 'm';
         break;
     }
     menu();
@@ -241,8 +319,8 @@ void serialRead() {
 
 void menu() {
   Serial.println("a:stop ");
-  Serial.println("b:auger");
-  Serial.println("c:plate");
-  Serial.println("d:measure");
+  Serial.println("k:auger");
+  Serial.println("p:plate");
+  Serial.println("m:measure");
 
 }
